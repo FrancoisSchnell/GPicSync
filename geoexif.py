e7a9683d9553c4ca742eb3b4829d1cdc566d9a89
francois.schnell <francois.schnell@1639a297-812b-0410-820f-b71bd62439dd>
2007-03-22 20:59:25 +0000

    integrated Marc Nozell patch for crosspteform compatibility with exiftool (windows-linux)

--- geoexif.py
+++ geoexif.py
@@ -6,13 +6,16 @@
 # (c) francois.schnell  francois.schnell@gmail.com
 #                       http://francois.schnell.free.fr 
 #  
-# This script is released under the GPL license
+# This script is released under the GPL license v2
+#
+# Integrated patch from Marc Nozell (http://nozell.com/blog):
+# for multi-OS compatibility with exiftool (windows-linux) 
 #
 # This script use the GPL exiftool.exe  app. see: 
 # http://www.sno.phy.queensu.ca/%7Ephil/exiftool/
 ###############################################################################
 
-import os
+import os,sys
 
 class GeoExif(object):
     """
@@ -21,10 +24,14 @@ class GeoExif(object):
     """
     def __init__(self,picture):
         self.picPath=picture
+        if sys.platform == 'win32':
+            self.exifcmd = 'exiftool.exe'
+        else:
+            self.exifcmd = 'exiftool'
         
     def readExifAll(self):
         """read all exif tags and return a string of the result"""
-        result=os.popen('exiftool.exe "%s"' % self.picPath).read()
+        result=os.popen('%s "%s"' % (self.exifcmd, self.picPath)).read()
         return result
     
     def readDateTime(self):
@@ -34,14 +41,13 @@ class GeoExif(object):
         like  ['2007:02:12', '16:09:10']
         """
         #result=os.popen('exiftool.exe -CreateDate "%s"' % self.picPath).read()
-        result=os.popen('exiftool.exe -DateTimeOriginal "%s"' % self.picPath).read()
+        result=os.popen('%s -DateTimeOriginal "%s"' % (self.exifcmd, self.picPath)).read()
         timeDate= [result[34:44],result[45:53]]
         return timeDate
     
     def readLatitude(self):
         """read the latitute tag is available and return a float"""
-        result=os.popen('exiftool.exe -n -GPSLatitude -GPSLatitudeRef  "%s"  '
-        % self.picPath).read().split("\n")
+        result=os.popen('%s -n -GPSLatitude -GPSLatitudeRef  "%s"  ' % (self.exifcmd, self.picPath)).read().split("\n")
         print result
         if len(result)>1:
             latitude=float(result[0].split(":")[1])
@@ -52,8 +58,7 @@ class GeoExif(object):
         
     def readLongitude(self):
         """read the longitude tag if available"""
-        result=os.popen('exiftool.exe -n -GPSLongitude -GPSLongitudeRef   "%s" '
-        % self.picPath).read().split("\n")
+        result=os.popen('%s -n -GPSLongitude -GPSLongitudeRef   "%s" ' % (self.exifcmd, self.picPath)).read().split("\n")
         print result
         if len(result)>1:
             longitude=float(result[0].split(":")[1])
@@ -72,10 +77,10 @@ class GeoExif(object):
          
         #os.popen('exiftool.exe -GPSAltitudeRef=0 -GPSAltitude=100 '+ self.picPath)
         if float(lat) >= 0:
-            os.popen('exiftool.exe  -GPSLatitudeRef="N" "%s" '% self.picPath)
+            os.popen('%s -GPSLatitudeRef="N" "%s" '% (self.exifcmd, self.picPath))
         else:
-            os.popen('exiftool.exe  -GPSLatitudeRef="S" "%s" '% self.picPath)    
-        os.popen('exiftool.exe  -GPSLatitude=%s "%s"'%(lat,self.picPath))
+            os.popen('%s -GPSLatitudeRef="S" "%s" '% (self.exifcmd, self.picPath))    
+        os.popen('%s  -GPSLatitude=%s "%s"'%(self.exifcmd, lat,self.picPath))
         
     def writeLongitude(self,long):
         """
@@ -85,32 +90,27 @@ class GeoExif(object):
         long can be a float or a string
         """
         if float(long) >= 0:
-            os.popen('exiftool.exe  -GPSLongitudeRef="E" "%s"' % self.picPath)
+            os.popen('%s -GPSLongitudeRef="E" "%s"' % (self.exifcmd, self.picPath))
         else:
-            os.popen('exiftool.exe  -GPSLongitudeRef="W" "%s"' % self.picPath)  
-        os.popen('exiftool.exe  -GPSLongitude=%s  "%s" '% (long,self.picPath))
+            os.popen('%s -GPSLongitudeRef="W" "%s"' % (self.exifcmd, self.picPath))
+        os.popen('%s -GPSLongitude=%s  "%s" '% (self.exifcmd, long,self.picPath))
         
     def writeLatLong(self,lat,long,latRef,longRef):
         """Write both latitudeRef/latitude and longitudeRef/longitude in EXIF"""
-        os.popen('exiftool.exe  -GPSLongitude=%s -GPSLatitude=%s \
-        -GPSLongitudeRef=%s -GPSLatitudeRef=%s  "%s"'%(long,lat,longRef,latRef,self.picPath))
+        os.popen('%s -GPSLongitude=%s -GPSLatitude=%s \
+        -GPSLongitudeRef=%s -GPSLatitudeRef=%s  "%s"'%(self.exifcmd, long,lat,longRef,latRef,self.picPath))
 
 if __name__=="__main__":
     
     mypicture=GeoExif("test.jpg")
-    #exif=mypicture.readExifAll()
-    #mypicture.writeLongitude(7.222333)
-    #mypicture.writeLatitude(48.419973)
-    #dateAndTime= mypicture.readDateTime()
-    mypicture.writeLatLong(7.222333,48.419973,"N","E")
+#    exif=mypicture.readExifAll()
+#    mypicture.writeLongitude(7.222333)
+#    mypicture.writeLatitude(48.419973)
+    dateAndTime= mypicture.readDateTime()
+#    mypicture.writeLatLong(7.222333,48.419973,"N","E")
     latitude=mypicture.readLatitude()
     longitude=mypicture.readLongitude()
-    #print "dateAndTime= ",dateAndTime
-    #print "latitude= ",latitude
-    #print "longitude= ",longitude
-    #print "EXIF= ", exif
-
-    
-    
-    
-
+    print "dateAndTime= ",dateAndTime
+    print "latitude= ",latitude
+    print "longitude= ",longitude
+    print "EXIF= ", exif
