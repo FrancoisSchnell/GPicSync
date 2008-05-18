8fbc2d10609d3626c9b8df0031f73cf74c35d106
francois.schnell <francois.schnell@1639a297-812b-0410-820f-b71bd62439dd>
2008-05-18 10:16:17 +0000

    Went back to rev832 for kmlGen - no unicode (looks like I'll have to change to unicode the all app. first, had problems with chinese folder's name reported by Fabien)

--- kmlGen.py
+++ kmlGen.py
@@ -8,20 +8,20 @@
 #
 ###############################################################################
 
-import time
-from thread import start_new_thread
+
 
 from geoexif import *
 from gpx import *
+import time
+from thread import start_new_thread
         
 class KML(object):
     """
     A quick and dirty kml generator in progress for gpicsync
     (for live viewing in Google Earth)
     """
-    
     def __init__(self,fileName,name,url="",timeStampOrder=False,utc="0",eleMode=0,iconsStyle=0,gmaps=False):
-        self.f=open(fileName+".kml","wb")
+        self.f=open(fileName+".kml","w")
         self.url=url
         self.timeStampOrder=timeStampOrder
         self.eleMode=eleMode # Elevation mode
@@ -33,18 +33,13 @@ class KML(object):
         self.utcOffset=sign+str(abs(int(float(utc))))+":00"
         print ">>> self.utcOffest in kml for time stamps: ", self.utcOffset
         
-        #check if GM URL finishes with "/" and add it if necessary
-        if self.url != "":
-            if url[-1:]!="/":
-                self.url=self.url+"/"
-        
-        kmlHead_p1=u"""<?xml version="1.0" encoding="UTF-8"?>
+        kmlHead_p1="""<?xml version="1.0" encoding="UTF-8"?>
 <kml xmlns="http://earth.google.com/kml/2.1">
 <Document>
-<name>"""+unicode(name)+"""</name>"""
+<name>"""+name+"""</name>"""
 
         if (self.iconsStyle==0) and (gmaps==False):
-            kmlHead_p2=u"""
+            kmlHead_p2="""
 <Style id="hoverIcon0">
 <IconStyle>
 <scale>5.0</scale>
@@ -68,9 +63,9 @@ class KML(object):
 <styleUrl>#hoverIcon0</styleUrl>
 </Pair>
 </StyleMap>"""
-        else: kmlHead_p2=u""
+        else: kmlHead_p2=""
 
-        kmlHead_p3=u"""
+        kmlHead_p3="""
 <Style id="lineStyle">
 <PolyStyle>
 <color>3feeee17</color>
@@ -97,16 +92,13 @@ class KML(object):
 """
 #<href>root://icons/palette-4.png</href>
 
-        #self.f.write(kmlHead_p1+kmlHead_p2+kmlHead_p3)
-        kmlHead=kmlHead_p1+kmlHead_p2+kmlHead_p3
-        self.f.write(kmlHead.encode("utf-8"))
+        self.f.write(kmlHead_p1+kmlHead_p2+kmlHead_p3)
         
     def writeInKml(self,text):
         """
         Print the given string in the kml file
         """
-        #self.f.write(text)
-        self.f.write(text.encode("utf-8"))
+        self.f.write(text)
 
     def footerPlacemark(self,picName,type="GE"):
         """
@@ -118,28 +110,28 @@ class KML(object):
         
         for ext in [".mp3",".wma",".ogg",".wav"]:
             if os.path.exists(mediaFile+ext):
-                print "Found mediaFile= ",repr(mediaFile+ext)
+                print "Found mediaFile= ",mediaFile+ext
                 if type=="GE":
-                    pmDescriptionFooter=u"<br><br><a href='"+\
+                    pmDescriptionFooter="<br><br><a href='"+\
                     mediaFile+ext+"'>Play Audio</a>"
                 elif type=="GM":
-                    pmDescriptionFooter=u"<br><br><a href='"+\
+                    pmDescriptionFooter="<br><br><a href='"+\
                     self.url+os.path.basename(picName.split(".")[0])+ext+"'>Play Audio</a>"
                 
         for ext in [".wmv",".mov",".avi"]:
             if os.path.exists(mediaFile+ext):
-                print "Found mediaFile= ",repr(mediaFile+ext)
+                print "Found mediaFile= ",mediaFile+ext
                 if type=="GE":
-                    pmDescriptionFooter=u"<br><br><a href='"+\
+                    pmDescriptionFooter="<br><br><a href='"+\
                     mediaFile+ext+"'>Play Video</a>"
                 elif type=="GM":
-                    pmDescriptionFooter=u"<br><br><a href='"+\
+                    pmDescriptionFooter="<br><br><a href='"+\
                     self.url+os.path.basename(picName.split(".")[0])+ext+"'>Play Video</a>"
                     
         if os.path.exists(mediaFile+".txt"):
-            print "Found .txt file to add= ",repr(mediaFile)+".txt"
+            print "Found .txt file to add= ",mediaFile+".txt"
             fileHandle = open (mediaFile+".txt")
-            pmDescriptionFooter=pmDescriptionFooter+u"<br><br>"+fileHandle.read()
+            pmDescriptionFooter=pmDescriptionFooter+"<br><br>"+fileHandle.read()
             fileHandle.close() 
         
         return pmDescriptionFooter
@@ -156,8 +148,8 @@ class KML(object):
         
         if elevation=="" or elevation=="None": elevation="0"
         if self.timeStampOrder==True:
-            timeStamp1=u"<TimeStamp><when>"+timeStamp+self.utcOffset+"</when> </TimeStamp>\n"
-            timeStamp2=u"<TimeSpan><begin>"+timeStamp+self.utcOffset+"</begin></TimeSpan>\n"
+            timeStamp1="<TimeStamp><when>"+timeStamp+self.utcOffset+"</when> </TimeStamp>\n"
+            timeStamp2="<TimeSpan><begin>"+timeStamp+self.utcOffset+"</begin></TimeSpan>\n"
             timeStamp=timeStamp1
         else:
             timeStamp=""
@@ -184,23 +176,23 @@ class KML(object):
             lat=mypicture.readLatitude()
             long=mypicture.readLongitude()
         
-        pmHead=u"\n\n<Placemark>\n<name>"+\
+        pmHead="\n\n<Placemark>\n<name>"+\
         os.path.basename(picName)+"</name>\n"
         
         if self.eleMode==1 or self.eleMode==2:
-            eleAdd=u"\n<altitudeMode>absolute</altitudeMode>"
+            eleAdd="\n<altitudeMode>absolute</altitudeMode>"
         else: eleAdd=""
             
         if self.iconsStyle==1:
-            iconLook=u"<styleUrl>#camera</styleUrl>"
+            iconLook="<styleUrl>#camera</styleUrl>"
         elif self.iconsStyle==0: 
-            iconLook=u"<styleUrl>#defaultStyle1</styleUrl><Style><IconStyle><Icon><href>thumbs/thumb_"+\
+            iconLook="<styleUrl>#defaultStyle1</styleUrl><Style><IconStyle><Icon><href>thumbs/thumb_"+\
             os.path.basename(picName)+"</href></Icon></IconStyle></Style>"\
 
         #Adding a footer to the description
         pmDescriptionFooter=self.footerPlacemark(picName,type="GE")
         
-        pmDescription=u"<description><![CDATA["+\
+        pmDescription="<description><![CDATA["+\
         "<img src='"+self.url+os.path.basename(picName)+"' width='"+width+"' height='"+height+"'/>"+\
         pmDescriptionFooter+\
         "]]>\n</description>\n"+iconLook+\
@@ -208,13 +200,10 @@ class KML(object):
         "\n<coordinates>"+str(long)+","+str(lat)+","+elevation+\
         "</coordinates>\n</Point>\n"+timeStamp
         
-        pmTail=u"</Placemark>"
-        
-        #self.f.write(pmHead)
-        #self.f.write(pmDescription)
-        #self.f.write(pmTail)
-        pm=pmHead+pmDescription+pmTail
-        self.f.write(pm.encode("utf-8"))
+        pmTail="</Placemark>"
+        self.f.write(pmHead)
+        self.f.write(pmDescription)
+        self.f.write(pmTail)
         
     def placemark4Gmaps(self,picName="",lat="",long="",width="400",height="300",elevation=""):
         """
@@ -241,13 +230,13 @@ class KML(object):
             mypicture=GeoExif(picName)
             lat=mypicture.readLatitude()
             long=mypicture.readLongitude()
-        pmHead=u"\n\n<Placemark>\n<name>"+\
+        pmHead="\n\n<Placemark>\n<name>"+\
         os.path.basename(picName)+"</name>\n"
         iconLook="<styleUrl>#camera</styleUrl>"
         
         #Adding a footer to the description
         pmDescriptionFooter=self.footerPlacemark(picName,type="GM")        
-        pmDescription=u"<description><![CDATA["+\
+        pmDescription="<description><![CDATA["+\
         "<a href='"+self.url+os.path.basename(picName)+"' target='_blank'> <img src='"+\
         self.url+"thumbs/thumb_"+os.path.basename(picName)+"'/></a>"+\
         pmDescriptionFooter+\
@@ -256,36 +245,32 @@ class KML(object):
         "\n<Point>"+\
         "\n<coordinates>"+str(long)+","+str(lat)+","+elevation+\
         "</coordinates>\n</Point>\n"
-        pmTail=u"</Placemark>"
-        #self.f.write(pmHead)
-        #self.f.write(pmDescription)
-        #self.f.write(pmTail)
-        pm=pmHead+pmDescription+pmTail
-        self.f.write(pm.encode("utf-8"))
+        pmTail="</Placemark>"
+        self.f.write(pmHead)
+        self.f.write(pmDescription)
+        self.f.write(pmTail)
 
     def path(self,gpxFile,cut=500):
         """ Creates the path of the GPX file in the kml"""
-        #self.f.write(u"\n<Folder>\n<name>Track</name>")
-        pathHead=u"\n<Folder>\n<name>Track</name>"
-        self.f.write(pathHead.encode("utf-8"))
+        self.f.write("\n<Folder>\n<name>Track</name>")
         i=1 # an iterator for the gpx file
         part=cut # cut the gpx file in part (to be sure it displays in GM)
         j=1 #Path j (a number for each section) 
         
         if self.eleMode==1:
-            pathAdd=u"\n<altitudeMode>absolute</altitudeMode>"
+            pathAdd="\n<altitudeMode>absolute</altitudeMode>"
         elif self.eleMode==2:
-            pathAdd=u"\n<altitudeMode>absolute</altitudeMode>\n<extrude>1</extrude>"
-        else: pathAdd=u""
+            pathAdd="\n<altitudeMode>absolute</altitudeMode>\n<extrude>1</extrude>"
+        else: pathAdd=""
         
         def makeHeadPath(j):
-            headPath=u"\n<Placemark>\n<name>Path "+str(j)+"</name>\n"\
+            headPath="\n<Placemark>\n<name>Path "+str(j)+"</name>\n"\
             +"<styleUrl>#lineStyle</styleUrl>\n<LineString>\n<tessellate>1</tessellate>"\
             +pathAdd+"\n<coordinates>\n"
             return headPath
         
-        endPath=u"\n</coordinates>\n</LineString>\n</Placemark>\n\n"
-        bodyPath=u""
+        endPath="\n</coordinates>\n</LineString>\n</Placemark>\n\n"
+        bodyPath=""
         myGpx=Gpx(gpxFile) 
         track=myGpx.extract()
                 
@@ -295,28 +280,24 @@ class KML(object):
                 bodyPath=bodyPath+rec['lon']+','+rec['lat']+','+rec['ele']+" "
                 i=i+1
             if i==part:
-                #self.f.write(makeHeadPath(j))
-                #self.f.write(bodyPath)
-                #self.f.write(endPath)
-                cont=u""+makeHeadPath(j)+bodyPath+endPath
-                self.f.write(cont.encode("utf-8"))
+                self.f.write(makeHeadPath(j))
+                self.f.write(bodyPath)
+                self.f.write(endPath)
                 i=1
                 j=j+1
                 bodyPath=""
 
-        #self.f.write(makeHeadPath(j))
-        #self.f.write(bodyPath)
-        #self.f.write(endPath)
-        #self.f.write("</Folder>\n")
-        content=makeHeadPath(j)+bodyPath+endPath+u"</Folder>\n"
-        self.f.write(content.encode("utf-8"))
+        self.f.write(makeHeadPath(j))
+        self.f.write(bodyPath)
+        self.f.write(endPath)
+        
+        self.f.write("</Folder>\n")
 
     def close(self):
         """Ending of the kml file"""
         print "close kml!"
-        kmlTail=u"\n</Document>\n</kml>"
-        #self.f.write(kmlTail)
-        self.f.write(kmlTail.encode("utf-8"))
+        kmlTail="\n</Document>\n</kml>"
+        self.f.write(kmlTail)
         self.f.close()
         
 if __name__=="__main__":
